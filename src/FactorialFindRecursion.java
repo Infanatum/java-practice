dd2ba0c468f13706c4bda5be1421c80dd325e3b4
Aleksei B <infanatum@gmail.com>
2018-08-19 23:00:45 +0300

    Create FactorialFindRecursion

---
+++ src/FactorialFindRecursion.java
@@ -0,0 +1,14 @@
+public class FactorialFindRecursion {
+    public static void main(String[] args) {
+        int factorial = factorial(5);
+        System.out.println(factorial);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
